1275e54046b30658fba8ab1fa9937525d813f312
Samlxrd <samlord.dzn@gmail.com>
2025-02-25 15:19:53 -0300

    Implementado Menu de Seleção de Ciclo, Tempo e Intervalo para o Timer do Pomodoro

--- pomodoro.c
+++ pomodoro.c
@@ -25,12 +25,24 @@
 uint32_t last_time;
 
 volatile bool cor = true;
+volatile bool active = false;
+
+// Predefinições de tempo que poderão ser escolhidas no programa
+static uint8_t cycles[] = { 2, 3, 4, 5 };
+static uint8_t work_time[] = { 20, 25, 30, 40, 50, 60 };
+static uint8_t break_time[] = { 5, 10, 15 };
+
+// Variáveis utilizadas para salvar as configurações escolhidas pelo usuário
+uint8_t ind_cycles, ind_work, ind_break;
+uint8_t selected_config;
+uint8_t indice;
 
 // Protótipo das Funções
 void setup(void);
 static void gpio_irq_handler(uint gpio, uint32_t events);
 uint pwm_init_gpio(uint gpio, uint wrap);
 int map(int valor, int center, int in_min, int in_max, int out_min, int out_max);
+void setup_menu(ssd1306_t ssd);
 
 int main()
 {
@@ -41,8 +53,6 @@ int main()
     gpio_set_irq_enabled(A_BUTTON, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(B_BUTTON, GPIO_IRQ_EDGE_FALL, true);
 
-    uint16_t adc_value_x;
-
     // Configuração inicial do display ssd1306, iniciado com todos os pixels apagados.
     ssd1306_t ssd; 
     ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
@@ -50,14 +60,23 @@ int main()
     ssd1306_send_data(&ssd);
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
+    
+    while(true) {
+        // Se o temporizador não estiver ativo, o usuário poderá selecionar as configurações de tempo desejadas.
+        if (!active) {
+            selected_config = 0;
+            indice = 0;
+            setup_menu(ssd);
+        }
 
-    while(true) {    
-
-        // Atualiza a tela do display
-        ssd1306_fill(&ssd, false); // Limpa a tela
-        ssd1306_rect(&ssd, 3, 3, 124, 60, cor, !cor);
-        ssd1306_send_data(&ssd); // Exibe na tela
-        sleep_ms(50);
+        // Temporizador em atividade
+        while (active) {
+            ssd1306_fill(&ssd, !cor); // Limpa o display
+            ssd1306_rect(&ssd, 3, 3, 124, 60, cor, !cor); // Desenha um retângulo
+            ssd1306_draw_string(&ssd, "ATIVO", 40, 10); // Desenha uma string
+            ssd1306_send_data(&ssd); // Atualiza o display
+        }
+            
     }
 
     return 0;
@@ -112,6 +131,25 @@ static void gpio_irq_handler(uint gpio, uint32_t events){
             break;
 
         case B_BUTTON:
+            if (!active) {
+                switch (selected_config) {
+                    case 0:
+                        ind_cycles = indice;
+                        indice = 0;
+                        selected_config += 1;
+                        break;
+                    case 1:
+                        ind_work = indice;
+                        indice = 0;
+                        selected_config += 1;
+                        break;
+                    case 2:
+                        ind_break = indice;
+                        indice = 0;
+                        selected_config += 1;
+                        break;
+                }
+            }
             break;
 
         case JOYSTICK_BUTTON:
@@ -136,4 +174,93 @@ uint pwm_init_gpio(uint gpio, uint wrap) {
 // Função para converter proporcionalmente os valores do adc para o display
 int map(int value, int center, int min, int max, int out_min, int out_max) {
     return ((value - center) * (out_max - out_min) / (max - min)) + (out_min + out_max) / 2;
+}
+
+void setup_menu(ssd1306_t ssd) {
+    char value[3];
+
+    uint16_t adc_value_x;
+
+    while(true)
+    {
+        if (!selected_config) {
+            adc_select_input(1);
+            adc_value_x = adc_read();
+
+            if (adc_value_x > 2700) {
+                if (indice == 3) { indice = 0; }
+                else { indice++; }
+            }
+
+            else if (adc_value_x < 1500) {
+                if (indice == 0) { indice = 3; }
+                else { indice--; }
+            }
+
+            sleep_ms(100);
+
+            sprintf(value, "%d", cycles[indice]);  // Converte o inteiro em string
+            ssd1306_fill(&ssd, !cor); // Limpa o display
+            ssd1306_rect(&ssd, 3, 3, 124, 60, cor, !cor); // Desenha um retângulo
+            ssd1306_draw_string(&ssd, "Ciclos", 40, 10); // Desenha uma string
+            ssd1306_draw_string(&ssd, value, 60, 30); // Desenha uma string
+            ssd1306_send_data(&ssd); // Atualiza o display
+        }
+
+        if (selected_config == 1) {
+            adc_select_input(1);
+            adc_value_x = adc_read();
+
+            if (adc_value_x > 2700) {
+                if (indice == 5) { indice = 0; }
+                else { indice++; }
+            }
+
+            else if (adc_value_x < 1500) {
+                if (indice == 0) { indice = 5; }
+                else { indice--; }
+            }
+
+            sleep_ms(100);
+
+            sprintf(value, "%d", work_time[indice]);  // Converte o inteiro em string
+            ssd1306_fill(&ssd, !cor); // Limpa o display
+            ssd1306_rect(&ssd, 3, 3, 124, 60, cor, !cor); // Desenha um retângulo
+            ssd1306_draw_string(&ssd, "Tempo", 44, 10); // Desenha uma string
+            ssd1306_draw_string(&ssd, value, 56, 30); // Desenha uma string
+            ssd1306_send_data(&ssd); // Atualiza o display
+        }
+    
+        if (selected_config == 2) {
+            adc_select_input(1);
+            adc_value_x = adc_read();
+
+            if (adc_value_x > 2700) {
+                if (indice == 2) { indice = 0; }
+                else { indice++; }
+            }
+
+            else if (adc_value_x < 1500) {
+                if (indice == 0) { indice = 2; }
+                else { indice--; }
+            }
+
+            sleep_ms(100);
+
+            sprintf(value, "%d", break_time[indice]);  // Converte o inteiro em string
+            ssd1306_fill(&ssd, !cor); // Limpa o display
+            ssd1306_rect(&ssd, 3, 3, 124, 60, cor, !cor); // Desenha um retângulo
+            ssd1306_draw_string(&ssd, "Pausa", 44, 10); // Desenha uma string
+            ssd1306_draw_string(&ssd, value, 60, 30); // Desenha uma string
+            ssd1306_send_data(&ssd); // Atualiza o display
+        }
+
+        sleep_ms(100);
+
+        if (selected_config == 3) {
+            // Iniciar o programa com as configurações predefinidas
+            active = true;
+            break;
+        }
+    }
 }
